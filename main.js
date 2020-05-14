197b05808ebb2d571459c353c7d02cf8ee5fb207
USER <k.k.fliper12@gmail.com>
2020-05-14 15:16:27 +0900

    top js 自動スライド

--- main.js
+++ main.js
@@ -29,25 +29,67 @@ selectElement('.close').addEventListener('click', () => {
 
 
 // top //
-var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// document.addEventListener('DOMContentLoaded', function(){
+//     const hero = new HeroSlider();
+// });
 
-    // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
+// class HeroSlider {
+//     constructor() {
+//         this.swiper = new Swiper ('.swiper-container', {
+//             // Optional parameters
+//             // direction: 'vertical',
+//             loop: true,
+//             grabCursor: true,
+//             effect: 'coverflow',
+//             centeredSlides: true,
+//             slidesPerViwe: 1,
+//             speed: 1000,
+//             autoplay: {
+//                 delay: 4000,
+//                 disableOnInteraction: false
+//             }
+//         })
+//     }
+// }
 
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-    el: '.swiper-scrollbar',
-    },
-})
+document.addEventListener('DOMContentLoaded', function(){
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+});
+
+class HeroSlider {
+    constructor(el) {
+        this.el = el;
+        this.swiper = this._initSwiper();
+    }
+
+    _initSwiper() {
+        return new Swiper (this.el, {
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
+            grabCursor: true,
+            effect: 'coverflow',
+            centeredSlides: true,
+            slidesPerViwe: 1,
+            speed: 1000,
+            autoplay: {
+                
+            }
+        });
+    }
+
+    start() {
+        this.swiper.params.autoplay = {
+            delay: 4000,
+            disableOnInteraction: false
+        }
+        this.swiper.autoplay.start();
+    }
+    stop() {
+        this.swiper.autoplay.stop();
+    }
+}
+
 // top //
