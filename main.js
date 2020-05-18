e99c2c9647fc6248783f24cf27394bc944091378
USER <k.k.fliper12@gmail.com>
2020-05-18 23:46:34 +0900

    header 変更過程

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 //============= header ============//
-// const selectElement = (s) => document.querySelector(s);
+class MobileMenu {
+    constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector('.mobile-menu__btn');
+    this.DOM.cover = document.querySelector('.mobile-menu__cover');
+    this.DOM.container = document.querySelector('#globle-container');
+    this.eventType = window.ontouchstart ? 'touchstart' : 'click';
+    this._addEvent();
+    }
 
-// selectElement('.open').addEventListener('click', () => {
-//     selectElement('.nav-list').classList.add('active');
-//     console.log("aaaaaaaaaaaa");
-// });
+    _getEventType() {
+        return window.ontouchstart ? 'touchstart' : 'click';
+    }
 
-// selectElement('.close').addEventListener('click', () => {
-//     selectElement('.nav-list').classList.remove('active');
-//     console.log("bbbbbbbbbbb");
-// })
-const header = document.querySelector('.hed');
-const _navAnimation = function(el, inview) {
-    if(inview) {
-        el.classList.remove('triggered');
-    } else {
-        el.classList.add('triggered');
+    _toggle() {
+        this.DOM.container.classList.toggle('menu-open');
+    }
+
+    _addEvent() {
+        this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+        this.DOM.cover.addEventListener('click', this._toggle.bind(this));
     }
 }
+
+new MobileMenu();
+
+
+// ===============Scroll==============//
+
+const hed = document.querySelector('.hed');
+const _navAnimation = function(el, inview) {
+    if(inview) {
+        hed.classList.remove('triggered');
+    } else {
+        hed.classList.add('triggered');
+    }
+}
+
+const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false})
+
 //=========== end-of-header ==============//
 
 
